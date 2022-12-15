8a117476aa3d3d77b369af852f7013199d127579
shirotabi <64385784+fuddt@users.noreply.github.com>
2022-12-15 21:32:00 +0900

    templatesでstaticまでやりました。次回Bootstrap

--- my_django/use_templates/urls.py
+++ my_django/use_templates/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('next_step', views.next_step, name='next_step'),
+    path('next_step_second', views.next_step_second, name='next_step_second'),
+    path('use_static', views.use_static, name='use_static'),
 ]
--- my_django/use_templates/views.py
+++ my_django/use_templates/views.py
@@ -3,4 +3,28 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-  return render(request, 'use_templates/index.html')
+	return render(request, 'use_templates/index.html')
+
+def next_step(request):
+	params_dict = {
+		'title': 'Next Step',
+		'msg' : 'テンプレートに変数を埋め込んでみる',
+  		'other_page':'next_step_second'
+	}
+	return render(request, 'use_templates/next_step.html', params_dict)
+
+def next_step_second(request):
+    params_dict = {
+		'title' : 'あなたが今見ているのはnext_step_second関数が実行された画面',
+		'msg'	: '同じhtmlページの変数の値が変わったページです',
+		'other_page':'next_step' 
+	}
+    return render(request, 'use_templates/next_step.html', params_dict)
+
+def use_static(request):
+    params_dict = {
+		'title' : 'templatesのstatic使いました。',
+		'msg'	: 'css記述するのめんどくさかったらBootstrap使ってね。',
+		'other_page':'next_step' 
+	}
+    return render(request, 'use_templates/use_static.html', params_dict)
